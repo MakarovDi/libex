c8ef5aab79df3627fce8ac50a9f3574a8e0b659b
Dmitry Makarov <MakarovDmV@gmail.com>
2015-04-05 12:55:58 +0300

    [platform] Compiler detection

--- src/platform.cpp
+++ src/platform.cpp
@@ -6,6 +6,76 @@ namespace ex
 {
 
 
+bool platform::is_unix()
+{
+#ifdef PLATFORM_IS_UNIX
+	return true;
+#else
+	return false;
+#endif
+}
+
+
+bool platform::is_windows()
+{
+#ifdef PLATFORM_IS_WINDOWS
+	return true;
+#else
+	return false;
+#endif
+}
+
+
+bool platform::is_linux()
+{
+#ifdef PLATFORM_IS_LINUX
+	return true;
+#else
+	return false;
+#endif
+}
+
+
+bool platform::is_bsd()
+{
+#ifdef PLATFORM_IS_BSD
+	return true;
+#else
+	return false;
+#endif
+}
+
+
+bool platform::is_apple()
+{
+#ifdef PLATFORM_IS_APPLE
+	return true;
+#else
+	return false;
+#endif
+}
+
+
+bool platform::is_posix()
+{
+#ifdef PLATFORM_IS_POSIX
+	return true;
+#else
+	return false;
+#endif
+}
+
+
+bool platform::is_android()
+{
+#ifdef PLATFORM_IS_ANDROID
+	return true;
+#else
+	return false;
+#endif
+}
+
+
 String platform::defs()
 {
 	String result = string::empty;
@@ -167,76 +237,5 @@ String platform::info()
 
 	return result;
 }
-
-
-bool platform::is_unix()
-{
-#ifdef PLATFORM_IS_UNIX
-	return true;
-#else
-	return false;
-#endif
-}
-
-
-bool platform::is_windows()
-{
-#ifdef PLATFORM_IS_WINDOWS
-	return true;
-#else
-	return false;
-#endif
-}
-
-
-bool platform::is_linux()
-{
-#ifdef PLATFORM_IS_LINUX
-	return true;
-#else
-	return false;
-#endif
-}
-
-
-bool platform::is_bsd()
-{
-#ifdef PLATFORM_IS_BSD
-	return true;
-#else
-	return false;
-#endif
-}
-
-
-bool platform::is_apple()
-{
-#ifdef PLATFORM_IS_APPLE
-	return true;
-#else
-	return false;
-#endif
-}
-
-
-bool platform::is_posix()
-{
-#ifdef PLATFORM_IS_POSIX
-	return true;
-#else
-	return false;
-#endif
-}
-
-
-bool platform::is_android()
-{
-#ifdef PLATFORM_IS_ANDROID
-	return true;
-#else
-	return false;
-#endif
-}
-
 
 }
