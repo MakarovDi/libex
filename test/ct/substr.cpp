147bc9ff380114671a00c9c930dd95d8577c7275
Dmitry Makarov <MakarovDmV@gmail.com>
2018-03-25 20:38:50 +0300

    [test] added test for ct/substr

--- test/ct/substr.cpp
+++ test/ct/substr.cpp
@@ -1,4 +1,23 @@
-//
-// Created by Dmitry on 25.03.2018.
-//
+#include <gtest/gtest.h>
 
+#include <ex/ct/substr>
+#include <ex/ct/streq>
+
+
+using namespace ex;
+
+
+TEST(ct_substr_test, substr_check)
+{
+    static_assert(ct::substr<2>("Test") == "st", "substr failure");
+    static_assert(ct::substr<2>(L"Test") == L"st", "substr failure");
+    static_assert(ct::substr<2>("Test") == ct::meta_str("st"), "substr failure");
+    static_assert(ct::substr<2>(L"Test") == ct::meta_str(L"st"), "substr failure");
+
+
+    static_assert(ct::substr<0>("Test") == "Test", "substr failure");
+    static_assert(ct::substr<0>(L"Test") == L"Test", "substr failure");
+
+    static_assert(ct::substr<4>("Test") == "", "substr failure");
+    static_assert(ct::substr<4>(L"Test") == L"", "substr failure");
+}
