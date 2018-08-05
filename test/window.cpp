9982dff8ab57f3ed9f4cb61132589fb950485b54
Dmitry Makarov <MakarovDi@yandex.ru>
2018-08-05 11:20:03 +0300

    [window] interface updated -> std iterator style

--- test/window.cpp
+++ test/window.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <ex/window>
+#include <array>
 
 
 TEST(window, min_window_3)
@@ -12,9 +13,9 @@ TEST(window, min_window_3)
     int expected_2[]  = { 7, 6, 5, 4, 3, 2, 1, 1, 1 };
     int expected_3[]  = { 2, 2, 2, 0, 0, 0, 2, 2, 2 };
 
-    ex::MinWindow<int> w1(3, test_data_1, 7);
-    ex::MinWindow<int> w2(3, test_data_2, 7);
-    ex::MinWindow<int> w3(3, test_data_3, 7);
+    ex::MinWindow<int> w1(3, test_data_1);
+    ex::MinWindow<int> w2(3, test_data_2);
+    ex::MinWindow<int> w3(3, test_data_3);
 
     for (int i = 0; i < 9; ++i)
     {
@@ -39,9 +40,9 @@ TEST(window, min_window_5)
     int expected_2[]  = { 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1 };
     int expected_3[]  = { 2, 2, 2, 0, 0, 0, 0, 0, 2, 2, 2 };
 
-    ex::MinWindow<int> w1(5, test_data_1, 7);
-    ex::MinWindow<int> w2(5, test_data_2, 7);
-    ex::MinWindow<int> w3(5, test_data_3, 7);
+    ex::MinWindow<int> w1(5, test_data_1);
+    ex::MinWindow<int> w2(5, test_data_2);
+    ex::MinWindow<int> w3(5, test_data_3);
 
     for (int i = 0; i < 11; ++i)
     {
@@ -61,7 +62,7 @@ TEST(window, min_window_large)
     int test_data[] = { 2, 2, 2, 0, 2, 2, 2 };
     int expected[]  = { 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2 };
 
-    ex::MinWindow<int> w1(8, test_data, 7);
+    ex::MinWindow<int> w1(8, test_data);
 
     for (int i = 0; i < 14; ++i)
     {
@@ -83,9 +84,9 @@ TEST(window, max_window_3)
     int expected_2[]  = { 7, 7, 7, 6, 5, 4, 3, 2, 1 };
     int expected_3[]  = { 2, 2, 2, 4, 4, 4, 2, 2, 2 };
 
-    ex::MaxWindow<int> w1(3, test_data_1, 7);
-    ex::MaxWindow<int> w2(3, test_data_2, 7);
-    ex::MaxWindow<int> w3(3, test_data_3, 7);
+    ex::MaxWindow<int> w1(3, test_data_1);
+    ex::MaxWindow<int> w2(3, test_data_2);
+    ex::MaxWindow<int> w3(3, test_data_3);
 
     for (int i = 0; i < 9; ++i)
     {
@@ -106,13 +107,14 @@ TEST(window, max_window_5)
     int test_data_2[] = { 7, 6, 5, 4, 3, 2, 1 };
     int test_data_3[] = { 2, 2, 2, 4, 2, 2, 2 };
 
-    int expected_1[]  = { 1, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7 };
-    int expected_2[]  = { 7, 7, 7, 7, 7, 6, 5, 4, 3, 2, 1 };
-    int expected_3[]  = { 2, 2, 2, 4, 4, 4, 4, 4, 2, 2, 2 };
+    int expected_1[]   = { 1, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7 };
+    int expected_2[]   = { 7, 7, 7, 7, 7, 6, 5, 4, 3, 2, 1 };
+    int expected_3[]   = { 2, 2, 2, 4, 4, 4, 4, 4, 2, 2, 2 };
+    int expected_cnt[] = { 1, 2, 3, 4, 5, 5, 5, 4, 3, 2, 1 };
 
-    ex::MaxWindow<int> w1(5, test_data_1, 7);
-    ex::MaxWindow<int> w2(5, test_data_2, 7);
-    ex::MaxWindow<int> w3(5, test_data_3, 7);
+    ex::MaxWindow<int> w1(5, test_data_1);
+    ex::MaxWindow<int> w2(5, test_data_2);
+    ex::MaxWindow<int> w3(5, test_data_3);
 
     for (int i = 0; i < 11; ++i)
     {
@@ -123,6 +125,10 @@ TEST(window, max_window_5)
         ASSERT_TRUE(w1.maximum() == expected_1[i]);
         ASSERT_TRUE(w2.maximum() == expected_2[i]);
         ASSERT_TRUE(w3.maximum() == expected_3[i]);
+
+        ASSERT_TRUE(w1.count() == expected_cnt[i]);
+        ASSERT_TRUE(w2.count() == expected_cnt[i]);
+        ASSERT_TRUE(w3.count() == expected_cnt[i]);
     }
 }
 
@@ -132,7 +138,45 @@ TEST(window, max_window_large)
     int test_data[] = { 2, 2, 2, 4, 2, 2, 2 };
     int expected[]  = { 2, 2, 2, 4, 4, 4, 4, 4, 4, 4, 4, 2, 2, 2 };
 
-    ex::MaxWindow<int> w1(8, test_data, 7);
+    ex::MaxWindow<int> w1(8, test_data);
+
+    for (int i = 0; i < 14; ++i)
+    {
+        w1.next();
+        ASSERT_TRUE(w1.maximum() == expected[i]);
+    }
+}
+
+
+TEST(window, class_check)
+{
+    std::array<int, 7> data = { 2, 2, 2, 4, 2, 2, 2 };
+    int expected[]  = { 2, 2, 2, 4, 4, 4, 4, 4, 4, 4, 4, 2, 2, 2 };
+
+    ex::MaxWindow<int> w1(8, data.begin(), data.end());
+
+    for (int i = 0; i < 14; ++i)
+    {
+        w1.next();
+        ASSERT_TRUE(w1.maximum() == expected[i]);
+    }
+}
+
+
+TEST(window, reset_check)
+{
+    std::array<int, 7> data = { 2, 2, 2, 4, 2, 2, 2 };
+    int expected[]  = { 2, 2, 2, 4, 4, 4, 4, 4, 4, 4, 4, 2, 2, 2 };
+
+    ex::MaxWindow<int> w1(8, data.begin(), data.end());
+
+    for (int i = 0; i < 7; ++i)
+    {
+        w1.next();
+        ASSERT_TRUE(w1.maximum() == expected[i]);
+    }
+
+    w1.reset();
 
     for (int i = 0; i < 14; ++i)
     {
